9ed6a444d02dc003be1dcf6ea9e283498e560411
anartovich@axway.com <anartovich@axway.com>
2023-07-13 11:35:55 -0500

    Modified 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxMDQxNg/spectral.js?branch_slug=main&token=28df1d13-aba6-432d-a860-335da87f1eab";
+export default { extends: ruleset };
